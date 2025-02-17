f55a0cff3e1db454d8c5363d09acad8b839203fa
Ethan Baird <ethanlewbaird@gmail.com>
2025-02-17 12:18:25 +0200

    fixed bugs

--- dwnstrm_CB.sh
+++ dwnstrm_CB.sh
@@ -14,16 +14,16 @@ OUTPUT_DIR="/data/ebaird/scRNAseq/dwnstrm_CB"
 
 INPUT_FILE="${INPUT_DIR}/2196_CB_output_filtered.h5"
 SAMPLE_OUTPUT_DIR="${OUTPUT_DIR}/2196_CB_output_filtered.h5"
-Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR
+Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR $2196
 
 INPUT_FILE="${INPUT_DIR}/2197_CB_output_filtered.h5"
 SAMPLE_OUTPUT_DIR="${OUTPUT_DIR}/2197_CB_output_filtered.h5"
-Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR
+Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR $2197
 
 INPUT_FILE="${INPUT_DIR}/2198_CB_output_filtered.h5"
 SAMPLE_OUTPUT_DIR="${OUTPUT_DIR}/2198_CB_output_filtered.h5"
-Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR
+Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR $2198
 
 INPUT_FILE="${INPUT_DIR}/2199_CB_output_filtered.h5"
 SAMPLE_OUTPUT_DIR="${OUTPUT_DIR}/2199_CB_output_filtered.h5"
-Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR
+Rscript --vanilla /data/ebaird/scRNAseq/dwnstrm_CB/dwnstrm_CB.r $INPUT_FILE $SAMPLE_OUTPUT_DIR $2199
